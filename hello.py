24b9e840ee797916b8bf10d06d0ea167319f172a
Pedrommano <160624863+Pedrommano@users.noreply.github.com>
2024-10-10 21:27:46 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,20 +1,36 @@
-from flask import Flask, request
 
-app = Flask(__name__)
-
+from flask import Flask, request, make_response, redirect, abort;
+app = Flask(__name__);
 
 @app.route('/')
-def hello_world():
-    return '<h1>Avaliação contínua: Aula 030</h1><ul><li><a href="/">Home</a></li><li><a href="/user/Fabio%20Teixeira/PT23820X/IFSP">Identificação</a></li><li><a href="/contextorequisicao">Contexto da requisição</a></li></ul>'
+def index():
+    
+    html_texto = '<h1>Avaliação contínua: Aula 030</h1>';    
+    html_texto += '<ul>';    
+    html_texto += '<li><a href="/">Home</a>';
+    html_texto += '<li><a href="/user/Fabio Teixeira/PT23820X/IFSP">Identificação</a>';
+    html_texto += '<li><a href="/contextorequisicao">Contexto da requisição</a>';
+    html_texto += '</ul>';
+    
+    return html_texto;
 
-@app.route('/user/<name>/<register>/<institution>')
-def user(name, register, institution):
-    return '<h1>Avaliação Contínua: Aula 030</h1><h1>Aluno: {n}</h1><h1>Prontuário: {r}</h1><h1>Instituição: {i}</h1> <a href="/">Voltar</a>'.format(n = name, r=register, i=institution)
- 
+@app.route('/user/<name>/<prontuario>/<institution>')
+def user(name, prontuario, institution):
+    html_texto = '<h1>Avaliação contínua: Aula 030</h1>';    
+    html_texto += '<h2>Aluno: {}</h2>'.format(name);
+    html_texto += '<h2>Prontuário: {}</h2>'.format(prontuario);
+    html_texto += '<h2>Instituição: {}</h2>'.format(institution);
+    html_texto += '<p><a href="/">Voltar</a></p>';
+
+    return html_texto;
 
 @app.route('/contextorequisicao')
-def contexto_requisicao():
-  browser = request.headers.get('User-Agent')
-  ip = request.remote_addr
-  host = request.headers.get('Host')
-  return '<h1>Avaliação Contínua: Aula 030</h1><h1>Seu navegador é: {}</h1><h1>O IP do computador remoto é: {}</h1><h1>O host da aplicação é: {}</h1><a href="/">Voltar</a>'.format(browser, ip, host)
+def contextorequisicao():
+    user_agent = request.headers.get('User-Agent');
+    html_texto = '<h1>Avaliação contínua: Aula 030</h1>';    
+    html_texto += '<h2>Seu navegador é: {}</h2>'.format(user_agent);
+    html_texto += '<h2>O IP do cumputador remoto é: {}</h2>'.format(request.remote_addr);
+    html_texto += '<h2>O host da aplicação é: {}</h2>'.format(request.host);
+    html_texto += '<p><a href="/">Voltar</a></p>';
+
+    return html_texto;
