ec97748a45f6d99516a856e8645a10bd0d1e40ad
Pedrommano <160624863+Pedrommano@users.noreply.github.com>
2024-10-16 20:21:43 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,36 +1,27 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+# from datetime import datetime, UTC
+from datetime import datetime
 
-from flask import Flask, request, make_response, redirect, abort;
-app = Flask(__name__);
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
-def index():
-    
-    html_texto = '<h1>Avaliação contínua: Aula 030</h1>';    
-    html_texto += '<ul>';    
-    html_texto += '<li><a href="/">Home</a>';
-    html_texto += '<li><a href="/user/Fabio Teixeira/PT23820X/IFSP">Identificação</a>';
-    html_texto += '<li><a href="/contextorequisicao">Contexto da requisição</a>';
-    html_texto += '</ul>';
-    
-    return html_texto;
+def hello_world():
+   now = datetime.utcnow()
+   return render_template('index.html', current_time=now)
 
-@app.route('/user/<name>/<prontuario>/<institution>')
-def user(name, prontuario, institution):
-    html_texto = '<h1>Avaliação contínua: Aula 030</h1>';    
-    html_texto += '<h2>Aluno: {}</h2>'.format(name);
-    html_texto += '<h2>Prontuário: {}</h2>'.format(prontuario);
-    html_texto += '<h2>Instituição: {}</h2>'.format(institution);
-    html_texto += '<p><a href="/">Voltar</a></p>';
 
-    return html_texto;
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
-@app.route('/contextorequisicao')
-def contextorequisicao():
-    user_agent = request.headers.get('User-Agent');
-    html_texto = '<h1>Avaliação contínua: Aula 030</h1>';    
-    html_texto += '<h2>Seu navegador é: {}</h2>'.format(user_agent);
-    html_texto += '<h2>O IP do cumputador remoto é: {}</h2>'.format(request.remote_addr);
-    html_texto += '<h2>O host da aplicação é: {}</h2>'.format(request.host);
-    html_texto += '<p><a href="/">Voltar</a></p>';
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
-    return html_texto;
+@app.errorhandler(500)
+def page_internal_server_error(e):
+    return render_template('500.html'), 500
